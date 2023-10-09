5d4998ba80c6306b227d3fa6a22b27fa9ddeff03
Tatewaki11 <144411796+Tatewaki11@users.noreply.github.com>
2023-10-09 10:42:40 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>7章課題</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $item_data = ['name' => 'orion', 'price' => 200, 'weight' => 160];
+
+      print_r($item_data);
+      ?>
+    </p>
+  </body>
+</html>
