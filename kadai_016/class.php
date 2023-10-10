ebebe47e441ccd6245f9cce147fc3982a8ebc788
Tatewaki11 <144411796+Tatewaki11@users.noreply.github.com>
2023-10-10 15:47:03 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>16章課題</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      class Food {
+        private $name;
+        private $price;
+        
+        public function show_price() {
+          echo $this->price . '<br>';
+        }
+
+        public function __construct(string $name, int $price) {
+          $this->name = $name;
+          $this->price = $price;
+        }
+      }
+
+      class Animal {
+        private $name;
+        private $height;
+        private $weight;
+
+        public function show_height() {
+          echo $this->height . '<br>';
+        }
+
+        public function __construct(string $name, int $height, int $weight) {
+          $this->name = $name;
+          $this->height = $height;
+          $this->weight = $weight;
+        }
+      }
+
+      $Food = new Food('potato', 250);
+      print_r($Food);
+      print_r('<br>');
+      $Animal = new Animal('dog', 60, 5000);
+      print_r($Animal);
+      print_r('<br>');
+
+      $Food->show_price();
+      $Animal->show_height();
+      ?>
+    </p>
+  </body>
+</html>
